d62776d4e4bd6c8f0ee05c977ec6c4a4da43099b
Aravind Ramanathan <ara@stripe.com>
2025-04-05 15:53:55 -0700

    init commit

---
+++ app/api/money/route.ts
@@ -0,0 +1,42 @@
+import { create } from "domain";
+import { NextResponse } from "next/server";
+import Stripe from "stripe";
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+
+export async function GET() {
+  const account = await stripe.v2.core.accounts.create({
+    contact_email: "jenny.rosen@example.com",
+    display_name: "Ara demo app PUB PREVIEW",
+    identity: {
+      country: "us",
+      entity_type: "individual",
+    },
+    configuration: {
+      recipient: {
+        capabilities: {
+          bank_accounts: {
+            local: {
+              requested: true,
+            },
+          },
+        },
+      },
+    },
+    include: ["identity", "configuration.recipient", "requirements"],
+  });
+
+  const accountLink = await stripe.v2.core.accountLinks.create({
+    account: account.id,
+    use_case: {
+      type: "account_onboarding",
+      account_onboarding: {
+        configurations: ["recipient"],
+        return_url: "https://example.com/return",
+        refresh_url: "https://example.com/reauth",
+      },
+    },
+  });
+  return NextResponse.json({
+    url: accountLink.url,
+  });
+}
