e328e9ddd1d5a24a510c70c26f42fd22b3ff7923
Lisa Li <lisali@stripe.com>
2025-10-16 18:59:30 -0400

    update api version

--- app/api/money/route.ts
+++ app/api/money/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2025-03-31.preview",
+  apiVersion: "2025-08-27.preview",
 });
 
 export async function GET() {
--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -4,7 +4,7 @@ import Stripe from "stripe";
 import { onboardedAccounts } from "../../lib/db";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2025-03-31.preview",
+  apiVersion: "2025-08-27.preview",
 });
 
 const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET!;
